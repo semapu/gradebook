1a36693e1b1509c328ce82a2b0680798d91cd576
unknown <sergimasp@gmail.com>
2021-07-10 17:37:12 +0200

    Looping and jumping

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -31,16 +31,66 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach(double grade in grades)  // To loop through a colleciton.
+            /* Using FORECH */
+            // foreach(double grade in grades)  // To loop through a colleciton.
+            // {   
+            //     result.High = Math.Max(grade, result.High);
+            //     result.Low = Math.Min(grade, result.Low);
+            //     result.Average += grade;
+            // }
 
-            {   
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
-            }
+            /* Using DO - WHILE. The first iteration is always done (there is no conditio to check at the beginning*/
+            // var index = 0;
+            // do
+            // {
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.Average += grades[index];
+
+            //     index += 1;
+
+            // } while(index < grades.Count);
+
+            /* Using WHILE */
+            // var index = 0;
+            // while(index < grades.Count)
+            // {
+            //     result.High = Math.Max(grades[index], result.High);
+            //     result.Low = Math.Min(grades[index], result.Low);
+            //     result.Average += grades[index];
+
+            //     index += 1;
+
+            // } 
+
+            /* Using FOR. Statements separated by ; */
+            for(var index = 0; index < grades.Count; index++)
+            {
+                /* Continu - Break example */
+                if(grades[index] == 42.1)  // Simple example. No reason behind.
+                {
+                    // break;
+                    continue;  // For instance, if we do not want to count this grade.
+                    index += 1;
+                }
+
+                /* GOTO exmaple. Nowadays it is rarely used.*/
+                if(grades[index] == 41.2)  // Simple example. No reason behind.
+                {
+                    goto done;  // This forces the code to jump to the label.
+                }
+
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+
+                index += 1;
+
+            } 
 
             result.Average /= grades.Count;
 
+            done:
             return result;
         }
 
