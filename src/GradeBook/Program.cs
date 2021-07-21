eae845ae758be3d23857e96fb06a7c25cec54e86
unknown <sergimasp@gmail.com>
2021-07-21 16:15:38 +0200

    Cathcing possible exception due to invalid input grades or format issues.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,10 +9,44 @@ namespace GradeBook
             // Create the new object.
             var book = new Book("Sergi's grade book");
             
-            // Methods used to interact with the object.
-            book.AddGrade(89.1);
-            book.AddGrade(98.1);
-            book.AddGrade(74.61);
+            // book.AddGrade(89.1);
+            // book.AddGrade(98.1);
+            // book.AddGrade(74.61);
+
+            // Input grades from the console
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to exit: ");
+                var input = Console.ReadLine();
+                
+                if(input == "q")  // Required the "" to indicate the comparition with an string
+                {
+                    break;
+                }
+                
+                // Ensuring the input from the user is valid. Exception thrown in Book.AddGrade().
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                // Catching specifically the exception of interest. Invalid argument.
+                catch(ArgumentException ex)
+                {
+                    //  Due to we have catch the exception, we can continue with the excecution.
+                    Console.WriteLine(ex.Message);
+                }
+                // Catching a possible exception due the converting the input string into double (from input to grade).
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                // Piece of code you always want to execute.
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
 
             var stats = book.GetStatistics();   
 
